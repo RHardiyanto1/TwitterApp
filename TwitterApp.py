8491c69d5ae4e2d06d241d3a22fa6057064cb596
RHardiyanto1 <47483898+RHardiyanto1@users.noreply.github.com>
2020-06-17 14:14:49 -0400

    Initial API Access

---
+++ TwitterApp.py
@@ -0,0 +1,12 @@
+import tweepy as tw
+import os
+
+consumer_key = os.environ['consumer_key']
+consumer_secret = os.environ['consumer_secret']
+access_token = os.environ['access_token']
+access_token_secret = os.environ['access_token_secret']
+
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
